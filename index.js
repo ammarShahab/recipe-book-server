9b10b41216a959c89005733c17cbd1a77e6c87ec
Ashahab007 <ashahab007@gmail.com>
2025-05-21 17:12:21 +0600

    created post api to save the data in db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,11 +11,14 @@ app.use(express.json());
 /* 
 User: dishdiarydb
 password: iGJMt4UGMOlR5l1C
+
+.env
+DB_USER=dishdiarydb
+DB_PASS=iGJMt4UGMOlR5l1C
 */
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://dishdiarydb:iGJMt4UGMOlR5l1C@cluster0.bmunlsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmunlsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +33,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const recipesCollections = client.db("dishdiarydb").collection("recipes");
+
+    app.post("/recipes", async (req, res) => {
+      const newRecipe = req.body;
+      console.log(newRecipe);
+      const result = await recipesCollections.insertOne(newRecipe);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -36,7 +50,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
