320666217aa642592e5ebebbdcb2b7ad53c87c7f
Ashahab007 <ashahab007@gmail.com>
2025-05-21 20:19:19 +0600

    created get api to send the data to recipe datails page

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ DB_USER=dishdiarydb
 DB_PASS=iGJMt4UGMOlR5l1C
 */
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bmunlsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -40,6 +40,18 @@ async function run() {
       const newRecipe = req.body;
       console.log(newRecipe);
       const result = await recipesCollections.insertOne(newRecipe);
+      res.send(result);
+    });
+
+    app.get("/recipes", async (req, res) => {
+      const result = await recipesCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await recipesCollections.findOne(query);
       res.send(result);
     });
 
