aa0b06f0ed335e876f6cf686cc1cbbe541cdc8f1
Ashahab007 <ashahab007@gmail.com>
2025-05-23 12:27:21 +0600

    show my recipes to ui

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete api
+    app.delete("/recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await recipesCollections.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
